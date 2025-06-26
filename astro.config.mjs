12fde6d1d273884a7454de1682ed46b994de93bd
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-06-26 11:19:05 +0300

    revert base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://akikurvinen.github.io",
-  base: "game-recs-app",
+  base: "astro-test",
   integrations: [preact()]
 });
